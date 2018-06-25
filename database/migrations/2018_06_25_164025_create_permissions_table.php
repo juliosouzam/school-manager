f988a2a393d4b984471c3b22d76bbaa0d6271729
Júlio César <julio.souzam@gmail.com>
2018-06-25 13:43:32 -0300

    add docker success

---
+++ database/migrations/2018_06_25_164025_create_permissions_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePermissionsTable extends Migration
+{
+    /**
+    * Run the migrations.
+    *
+    * @return void
+    */
+    public function up()
+    {
+        Schema::create('permissions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('slug');
+            $table->unsignedInteger('group_id');
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('group_id')->references('id')->on('groups')->onDelete('cascade');
+        });
+
+        Schema::create('permission_role', function (Blueprint $table) {
+            $table->integer('permission_id')->unsigned();
+            $table->integer('role_id')->unsigned();
+
+            $table->foreign('permission_id')->references('id')->on('permissions')->onDelete('cascade');
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+        });
+    }
+
+    /**
+    * Reverse the migrations.
+    *
+    * @return void
+    */
+    public function down()
+    {
+        Schema::dropIfExists('permissions');
+    }
+}
