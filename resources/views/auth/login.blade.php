1f04ece2e612b835c3f8310ba900ea6d6c90c089
Júlio César <julio.souzam@gmail.com>
2018-06-26 17:06:32 -0300

    update screen page login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,20 +1,42 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @stack('styles')
+</head>
+<body>
+    <div class="container mt-5">
+        <div class="row justify-content-md-center">
+            <div class="col-md-6">
                 <div class="card">
-                    <div class="card-header">{{ __('Login') }}</div>
+                    <div class="card-header bg-primary text-center text-white">
+                        <h5>
+                            {{ __('School Manager') }}
+                        </h5>
+                    </div>
 
-                    <div class="card-body">
+                    <div class="card-body justify-content-md-center">
                         <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
                             @csrf
 
-                            <div class="form-group row">
-                                <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-mail') }}</label>
+                            <div class="form-group row offset-1">
+                                <label for="email" class="col-sm-4 col-form-label">{{ __('E-mail') }}</label>
 
-                                <div class="col-md-6">
+                                <div class="col-md-10">
                                     <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
 
                                     @if ($errors->has('email'))
@@ -25,10 +47,10 @@
                                 </div>
                             </div>
 
-                            <div class="form-group row">
-                                <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Senha') }}</label>
+                            <div class="form-group row offset-1">
+                                <label for="password" class="col-md-4 col-form-label">{{ __('Senha') }}</label>
 
-                                <div class="col-md-6">
+                                <div class="col-md-10">
                                     <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
 
                                     @if ($errors->has('password'))
@@ -38,8 +60,8 @@
                                     @endif
                                 </div>
                             </div>
-                            <div class="form-group row">
-                                <div class="col-md-6 offset-md-4">
+                            <div class="form-group row offset-1">
+                                <div class="col-md-6">
                                     <div class="checkbox">
                                         <label>
                                             <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('Lembrar me') }}
@@ -48,15 +70,11 @@
                                 </div>
                             </div>
 
-                            <div class="form-group row mb-0">
-                                <div class="col-md-8 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
+                            <div class="form-group row mb-0 offset-1">
+                                <div class="col-md-10">
+                                    <button type="submit" class="btn btn-primary btn-block">
                                         {{ __('Entrar') }}
                                     </button>
-
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Esqueceu sua senha?') }}
-                                    </a>
                                 </div>
                             </div>
                         </form>
@@ -65,4 +83,7 @@
             </div>
         </div>
     </div>
-@endsection
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    @stack('scripts')
+</body>
+</html>
