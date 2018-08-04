d6a15a3b5fb83ff60a4ec1dacbd46c5c36f7e9e7
Júlio César <julio.souzam@gmail.com>
2018-08-04 11:25:15 -0300

    refact code test

--- tests/Unit/CourseTest.php
+++ tests/Unit/CourseTest.php
@@ -3,7 +3,6 @@
 namespace Tests\Unit;
 
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class CourseTest extends TestCase
@@ -29,6 +28,4 @@ class CourseTest extends TestCase
 
         $this->assertCount(2, $course->students);
     }
-
-
 }
